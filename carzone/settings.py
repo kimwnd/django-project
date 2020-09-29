6ed1f372591d4ba8fe314b678ffbf40c7ce48b58
kimwnd <gwcm@naver.com>
2020-09-29 12:51:07 +0900

    add database

--- carzone/settings.py
+++ carzone/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -121,6 +121,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'carzon/static'),
 ]
+
+# Media setting
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL  = '/media/'
--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-# Create your views here.
-
+from .models import Team
+ 
+ 
 def home(request):
-    return render(request, 'pages/index.html')
+    teams = Team.objects.all()
+    data = {'teams': teams,}
+    
+    return render(request, 'pages/index.html', data)
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    teams = Team.objects.all()
+    data = {'teams': teams,}
+    
+    return render(request, 'pages/about.html', data)
 
 
 def services(request):
